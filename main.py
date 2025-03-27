cd07105ef8269e69a8fb3de5f532c503eb3a4da2
Moritz Sallermann <moritzsallermann@gmail.com>
2025-03-27 18:29:30 +0000

    main: pre-images of trefoil hopfion

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ plotter.enable_shadows()
 
 N_RES_CURVE = 500
 N_RES_PHI = 64
+INTERVAL = np.linspace(0, 2 * np.pi, N_RES_CURVE, endpoint=False)
 
 ## For quicker testing
 # N_RES_CURVE = 50
@@ -44,24 +45,54 @@ def color_callback(point, t, phi, n_twists):
 # ########################################
 #           Trefoil Hopfion
 # ########################################
+curve_func = util.trefoil
+RADIUS = 0.5
+N_TWISTS = 2
+
 trefoil_hopfion = util.tube_from_3D_curve(
-    util.trefoil,
-    interval=np.linspace(0, 2 * np.pi, N_RES_CURVE, endpoint=False),
-    radius=0.5,
+    curve_func,
+    interval=INTERVAL,
+    radius=RADIUS,
     resolution_phi=N_RES_PHI,
-    color_callback=lambda point, t, phi: color_callback(point, t, phi, n_twists=1),
+    color_callback=lambda point, t, phi: color_callback(
+        point, t, phi, n_twists=N_TWISTS
+    ),
 )
 plotter.add_mesh(trefoil_hopfion, **MESH_ARGS)
+
+PHI_LIST = np.linspace(0, 2 * np.pi, 16, endpoint=False)
+pre_images = util.create_preimage_meshes(
+    curve_func,
+    phi_list=PHI_LIST,
+    radius=RADIUS * 1.1,
+    tube_radius=0.035,
+    n_res_curve=N_RES_CURVE,
+    n_twists=N_TWISTS,
+    n_res_phi=N_RES_PHI,
+)
+
+for phi, m in zip(PHI_LIST, pre_images):
+    args = MESH_ARGS.copy()
+    args["rgb"] = False
+    args["color"] = util.get_rgba_color(np.array([np.cos(phi), np.sin(phi), 0]))
+    plotter.add_mesh(m, **args)
+
 
 # ########################################
 #         Toroidal Hopfion
 # ########################################
+curve_func = lambda t: util.ring(t, radius=1.5)
+N_TWISTS = 6
+RADIUS = 0.75
+
 toroidal_hopfion = util.tube_from_3D_curve(
-    lambda t: util.ring(t, radius=1.5),
-    interval=np.linspace(0, 2 * np.pi, N_RES_CURVE),
-    radius=0.75,
+    curve_func,
+    interval=INTERVAL,
+    radius=RADIUS,
     resolution_phi=N_RES_PHI,
-    color_callback=lambda point, t, phi: color_callback(point, t, phi, n_twists=6),
+    color_callback=lambda point, t, phi: color_callback(
+        point, t, phi, n_twists=N_TWISTS
+    ),
 )
 toroidal_hopfion.translate([7, 0, 0], inplace=True)
 plotter.add_mesh(toroidal_hopfion, **MESH_ARGS)
@@ -70,12 +101,17 @@ plotter.add_mesh(toroidal_hopfion, **MESH_ARGS)
 ########################################
 #        Skyrmion tube and Hopfion
 ########################################
+curve_func = lambda t: util.ring(t, radius=1.5)
+RADIUS = 0.75
+N_TWISTS = 1
 toroidal_hopfion_tube = util.tube_from_3D_curve(
-    lambda t: util.ring(t, radius=3),
-    interval=np.linspace(0, 2 * np.pi, N_RES_CURVE, endpoint=False),
-    radius=0.75,
+    curve_func,
+    interval=INTERVAL,
+    radius=RADIUS,
     resolution_phi=N_RES_PHI,
-    color_callback=lambda point, t, phi: color_callback(point, t, phi, n_twists=1),
+    color_callback=lambda point, t, phi: color_callback(
+        point, t, phi, n_twists=N_TWISTS
+    ),
 )
 toroidal_hopfion_tube.translate([-7, 0, 0], inplace=True)
 plotter.add_mesh(toroidal_hopfion_tube, **MESH_ARGS)
